199bc1bff43021f0255eabca9f29eb040338d4cc
Janssen <larsjanssen64@gmail.com>
2017-06-19 14:53:33 +0200

    Fix rel

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,7 +12,7 @@
 	<link href="{{ mix('/css/bulma.css') }}" rel="stylesheet">
 	<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ mix('/css/packages.css') }}" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
 	<!-- Scripts -->
 	<script>
